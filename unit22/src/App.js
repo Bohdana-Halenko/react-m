81c7420eb5349fdd0b1acaba904639b748cc252d
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2025-12-02 19:01:51 +0200

    Update App.js

--- unit22/src/App.js
+++ unit22/src/App.js
@@ -9,15 +9,15 @@
 
 // Тепер
 // import {rootReducer} from 'reducer';
-import { configureStore } from "@reduxjs/toolkit";
-import { tasksReducer, filtersReducer } from "./reducer";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { tasksReducer, filtersReducer } from "./reducer";
 
-export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    filters: filtersReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     tasks: tasksReducer,
+//     filters: filtersReducer,
+//   },
+// });
 
 //src/redux/reducer.js
 // Раніше
@@ -43,26 +43,26 @@ export const store = configureStore({
 // })
 
 // Тепер
-import { statusFilters } from "./constants";
-import { createReducer } from "@reduxjs/toolkit";
-import { addTasks, deleteTasks, toggleComplated } from "./actions";
-const tasksInitialState = [];
-export const tasksReducer = createReducer(tasksInitialState, {
-  [addTasks]: (state, action) => {
-    state.push(action.payload);
-  },
-  [deleteTasks]: (state, action) => {
-    const index = state.findIndex((task) => task.id === action.payload);
-    return state.splice(index, 1);
-  },
-  [toggleComplated]: (state, action) => {},
-});
-const filtersInitialState = {
-  status: statusFilters.all,
-};
-export const filtersReducer = (state = filtersInitialState, action) => {
-  //
-};
+// import { statusFilters } from "./constants";
+// import { createReducer } from "@reduxjs/toolkit";
+// import { addTasks, deleteTasks, toggleComplated } from "./actions";
+// const tasksInitialState = [];
+// export const tasksReducer = createReducer(tasksInitialState, {
+//   [addTasks]: (state, action) => {
+//     state.push(action.payload);
+//   },
+//   [deleteTasks]: (state, action) => {
+//     const index = state.findIndex((task) => task.id === action.payload);
+//     return state.splice(index, 1);
+//   },
+//   [toggleComplated]: (state, action) => {},
+// });
+// const filtersInitialState = {
+//   status: statusFilters.all,
+// };
+// export const filtersReducer = (state = filtersInitialState, action) => {
+//   //
+// };
 
 //src/redux/actions.js
 // Раніше
@@ -89,32 +89,156 @@ export const filtersReducer = (state = filtersInitialState, action) => {
 // export const setStatusFilter = createAction('tasks/setStatusFilter')
 // console.log(addTask('Hello'));
 
-import { createSlice, nanoid } from "@reduxjs/toolkit";
-const tasksSlice = createSlice({
-  name: "tasks",
-  initialState: tasksInitialState,
-  reducers: {
-    addTasks: {
-      reducer(state, action) {
-        state.push(action.payload);
-      },
-      prepare(text) {
-        return {
-          payload: {
-            text,
-            id: nanoid(),
-            complated: false,
-          },
-        };
-      },
-    },
+// import { createSlice, nanoid } from "@reduxjs/toolkit";
+// const tasksSlice = createSlice({
+//   name: "tasks",
+//   initialState: tasksInitialState,
+//   reducers: {
+//     addTasks: {
+//       reducer(state, action) {
+//         state.push(action.payload);
+//       },
+//       prepare(text) {
+//         return {
+//           payload: {
+//             text,
+//             id: nanoid(),
+//             complated: false,
+//           },
+//         };
+//       },
+//     },
 
-    deleteTasks(state, action) {
-      const index = state.findIndex((task) => task.id === action.payload);
-      state.splice(index, 1);
-    },
-  },
+//     deleteTasks(state, action) {
+//       const index = state.findIndex((task) => task.id === action.payload);
+//       state.splice(index, 1);
+//     },
+//   },
+// });
+
+// const { addTasks, deleteTasks } = tasksSlice.actions;
+// const tasksReducer = tasksSlice.reducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const tasksSlice = {
+//   name: 'tasks',
+//   initialState: {
+//     items: [],
+//     isLoading: false,
+//     error: null,
+//   },
+//   reducers: {
+// fetchingInProgress(state) {
+//   state.isLoading = true;
+// },
+// fetchingSuccess(state, action){
+//   state.isLoading = false;
+//   state.error = null;
+//   state.items = action.payload;
+// },
+// fetchingError(state, action){
+//   state.isLoading = false;
+//   state.error = action.paydoad;
+// },
+//   },
+// }
+
+// export const {fetchingInProgress, fetchingSuccess, fetchingError} = tasksSlice.actions;
+
+// import axios from 'axios';
+// import {fetchingInProgress, fetchingSuccess, fetchingError} from "./tasksSlice";
+
+
+// axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
+
+// const fetchTasks = () => async dispatch => {
+//   try{
+//     dispatch(fetchingInProgress());
+//     const res = await axios.get('./tasks');
+
+//     dispatch(fetchingSuccess(res.data));
+//   } 
+//   catch(error){
+//     dispatch(fetchingError(error.message))
+//   };
+// }
+
+
+// // APP.js
+
+// import {useEffect} from "react";
+// import {useDispatch, useSelector} from "react-redux";
+// import {fetchTasks} from "redux/operations";
+// import {getTasks} from "redux/selectors";
+
+// const App = () =>{
+//   const dispatch = useDispatch();
+//   const {items, isLoading, error} = useSelector(getTasks);
+//   useEffect(() => {
+//     dispatch(fetchTasks());
+//   }, [dispatch]);
+
+//   return(
+//     <div>
+//       {isLoading && <p>Loading...</p>}
+//       {error && <p>{error}</p>}
+//       <p>{items.length > 0 && JSON.stringify(items, null, false)}</p>
+//     </div>
+//   )
+// }
+
+
+import axios from 'axios';
+import {createAsyncThunk} from "@reduxjs/toolkit";
+axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
+
+const fetchTasks = createAsyncThunk('tasks/fetchAll', async (_, thunkAPI) =>{
+  try{
+    const res = await axios.get("/tasks");
+    return res.data;
+  }
+  catch(error){
+    return thunkAPI.rejectWithValue(error.message);
+  }
 });
 
-const { addTasks, deleteTasks } = tasksSlice.actions;
-const tasksReducer = tasksSlice.reducer;
+
+import {createSlice} from "@reduxjs/toolkit";
+
+const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState: {
+      items: [],
+      isLoading: false,
+      error: null,
+    },
+
+    extraReducers: {
+      [fetchTasks.pending](state, action){
+        state.isLoading = true;
+      },
+      [fetchTasks.fulfilled](state, action){
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      },
+      [fetchTasks.rejected](state, action){
+        state.isLoading = false;
+        state.error = action.payload;
+      },
+    }
+  });
+
+  export const tasksReducer = tasksSlice.reducer;
